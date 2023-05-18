efeba670be4c4c05e9c0eff1fe265e2c6966ade8
iafominykh <iafominykh@yandex.ru>
2023-05-18 11:26:13 +0300

    HW6

--- src/video.py
+++ src/video.py
@@ -3,14 +3,23 @@ from src.channel import Channel
 class Video(Channel):
     def __init__(self, video_id):
         self.video_id = video_id
-        youtube = self.get_service()
-        self.video = youtube.videos().list(part='snippet,statistics,contentDetails,topicDetails',
-                                           id=self.video_id
-                                           ).execute()
-        self.title = self.video['items'][0]['snippet']['title']
-        self.url = 'https://www.youtube.com/watch?v=' + self.video_id
-        self.view_count = self.video['items'][0]['statistics']['viewCount']
-        self.like_count = self.video['items'][0]['statistics']['likeCount']
+        try:
+            youtube = self.get_service()
+            self.video = youtube.videos().list(part='snippet,statistics,contentDetails,topicDetails',
+                                               id=self.video_id
+                                               ).execute()
+            self.title = self.video['items'][0]['snippet']['title']
+            self.url = 'https://www.youtube.com/watch?v=' + self.video_id
+            self.view_count = self.video['items'][0]['statistics']['viewCount']
+            self.like_count = self.video['items'][0]['statistics']['likeCount']
+        except IndexError:
+            print('Не верная ссылка!')
+            self.youtube = None
+            self.title = None
+            self.url = None
+            self.viev_count = None
+            self.like_count = None
+
 
     def __str__(self):
         return f'{self.title}'
